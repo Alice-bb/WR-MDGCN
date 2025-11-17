3b97ba0aa57e89f95d60bbf18a58542979e5f44e
Alice-bb <2121903712@qq.com>
2025-11-17 19:55:04 +0800

    Update dataloader.py

--- lib/dataloader.py
+++ lib/dataloader.py
@@ -9,31 +9,6 @@ from lib.normalization import NScaler, MinMax01Scaler, MinMax11Scaler, StandardS
 from sklearn.cluster import KMeans, DBSCAN, SpectralClustering
 
 
-# # 聚类分析时间段函数
-# def cluster_time_periods(time_features, n_clusters=4):
-#     """
-#     使用K-means对时间特征进行聚类，将时间段分为n_clusters类
-#     :param time_features: 时间特征数组，包含每天的时间和星期几等特征
-#     :param n_clusters: 聚类类别数，默认为4
-#     :return: 聚类标签和每个类别的具体时间段
-#     """
-#     # 使用KMeans对时间特征进行聚类
-#     kmeans = KMeans(n_clusters=n_clusters, random_state=42)
-#     cluster_labels = kmeans.fit_predict(time_features)
-#
-#     # 计算每个类的时间范围
-#     clusters = {}
-#     for i in range(n_clusters):
-#         cluster_indices = np.where(cluster_labels == i)[0]
-#         clusters[i] = cluster_indices
-#
-#     # 输出每类的具体时间段
-#     print("聚类结果：")
-#     for i, indices in clusters.items():
-#         print(f"类别 {i} 对应时间段：{indices}")
-#
-#     return cluster_labels, clusters
-
 #数据归一化函数
 def normalize_dataset(data, normalizer, column_wise=False):
     if normalizer == 'max01':
@@ -138,70 +113,10 @@ def get_dataloader(args, normalizer = 'std', tod=False, dow=False, weather=False
     # numerical day_in_week
     day_in_week = [(i // args.steps_per_day)%args.days_per_week for i in range(data.shape[0])]
     day_in_week = np.array(day_in_week)
-    # print("day_in_week shape:", day_in_week.shape)
     day_in_week = np.tile(day_in_week, [1, N, 1]).transpose((2, 1, 0))
     # print("day_in_week shape:", day_in_week.shape)
     #print("day_in_week的时间片设置:", day_in_week[:, :, 0])
     feature_list.append(day_in_week)
-
-    # workday_weekend = np.array(
-    #     [1 if (day % 7 < 5) else 0 for day in range(data.shape[0])])  # 0 for weekend, 1 for workday
-    # cluster_labels = np.tile(workday_weekend, [1, N, 1]).transpose((2, 1, 0))
-    # # Append to feature list
-    # feature_list.append(cluster_labels)
-
-    # cluster_labels = []
-    # cluster_counts = []
-    # num_clusters = 7  # 根据你的具体情况调整
-    # eps = 0.05  # DBSCAN的半径参数，可以根据你的数据调整
-    # min_samples = 1  # 最小样本数量参数，也可以根据数据调整
-    # #
-    # for i in range(data.shape[0]):
-    #     clusters = data[i,:,:]
-    #     # 设置聚类的数量
-    #     # # 使用 Spectral Clustering 进行聚类
-    #     # spectral = SpectralClustering(n_clusters=num_clusters, affinity='nearest_neighbors', n_neighbors=5)
-    #     # spectral.fit(clusters)  # 将流量信息传入谱聚类
-    #     #
-    #     # # 获取每个节点的聚类标签
-    #     # labels = torch.tensor(spectral.labels_)
-    #     # cluster_labels.append(labels)
-    #
-    #
-    #     # 使用 DBSCAN 进行聚类
-    #     db = DBSCAN(eps=eps, min_samples=min_samples)
-    #     db.fit(clusters)  # 将流量信息转换为 numpy 数组进行聚类
-    #
-    #     # 获取每个节点的聚类标签
-    #     labels = torch.tensor(db.labels_)  # 使用 db.labels_ 获取每个节点的聚类标签
-    #     # 计算簇的数量，去除噪声点 (-1) 后的唯一标签数量
-    #     unique_labels = set(labels) - {-1}  # 去除噪声点 (-1)
-    #     num_clusters = len(unique_labels)  # 唯一标签的数量即为簇的数量
-    #
-    #     # 将结果存储
-    #     cluster_counts.append(num_clusters)  # 保存簇数量
-    #     cluster_labels.append(labels)
-    #     #
-    # #     # # 使用 K-means 进行聚类
-    # #     # kmeans = KMeans(n_clusters=num_clusters)
-    # #     # kmeans.fit(clusters)  # 将流量信息转换为numpy数组进行聚类
-    # #     # # 获取每个节点的聚类标签
-    # #     # labels = torch.tensor(kmeans.labels_)
-    # #     # cluster_labels.append(labels)
-    # cluster_labels = torch.stack(cluster_labels,dim=0)
-    # print("cluster_labels1",cluster_labels.shape)
-    # cluster_labels = np.tile(cluster_labels, [1, 1, 1]).transpose((1, 2, 0))
-    # print("cluster_labels2 ",cluster_labels.shape)
-    # feature_list.append(cluster_labels)
-    # cluster_counts =  np.tile(cluster_counts, [1, N, 1]).transpose((2, 1, 0))
-    # print("cluster_counts ", cluster_counts.shape)
-    # feature_list.append(cluster_counts)
-# cluster_labels = np.array(cluster_labels)
-#     # print("cluster_labels",cluster_labels.shape)
-#     cluster_labels = np.tile(cluster_labels, [L, 1, 1]).transpose((0, 2, 1))
-#     # print("cluster_labels",cluster_labels.shape)
-#         # 添加聚类标签到特征列表
-#     feature_list.append(cluster_labels)  # 扩展维度以匹配其他特征
 
     speed = load_st_speed(args.dataset)
     speed = np.array(speed)
@@ -210,141 +125,6 @@ def get_dataloader(args, normalizer = 'std', tod=False, dow=False, weather=False
     occupy = load_st_occupy(args.dataset)
     occupy = np.array(occupy)
     feature_list.append(occupy)
-    # workday_weekend = [1 if (((i // args.steps_per_day)%args.days_per_week) % 7 < 5) else 0 for i in range(data.shape[0])]  # 0 for weekend, 1 for workday
-    # workday_weekend = np.array(workday_weekend)
-    # workday_weekend = np.tile(workday_weekend, [1, N, 1]).transpose((2, 1, 0))
-    # # Append to feature list
-    # feature_list.append(workday_weekend)
-
-    # # 计算过去一周与过去两周的同一时间片的流量平均值
-    # steps_per_week = args.steps_per_day * 7
-    # weekly_avg_feature = np.zeros((L, N, F))
-    # for i in range(data.shape[0]):
-    #     if i >= 4 * steps_per_week:
-    #         # 获取过去四周同一时间片的平均值
-    #         last_week_avg = data[i - steps_per_week, :, 0]
-    #         two_weeks_ago_avg = data[i - 2 * steps_per_week, :, 0]
-    #         three_weeks_ago_avg = data[i - 3 * steps_per_week, :, 0]
-    #         four_weeks_ago_avg = data[i - 4 * steps_per_week, :, 0]
-    #         last_day_avg = data[i - args.steps_per_day, :, 0]
-    #         two_days_ago_avg = data[i - 2 * args.steps_per_day, :, 0]
-    #         three_days_ago_avg = data[i - 3 * args.steps_per_day, :, 0]
-    #         four_days_ago_avg = data[i - 4 * args.steps_per_day, :, 0]
-    #         weekly_avg_feature[i, :, 0] = (
-    #                                               last_week_avg + two_weeks_ago_avg + three_weeks_ago_avg + four_weeks_ago_avg
-    #                                               +last_day_avg + two_days_ago_avg + three_days_ago_avg + four_days_ago_avg
-    #                                       ) / 8
-    #     elif i >= 3 * steps_per_week:
-    #         # 用前三周数据 + 前一天的数据
-    #         last_week_avg = data[i - steps_per_week, :, 0]
-    #         two_weeks_ago_avg = data[i - 2 * steps_per_week, :, 0]
-    #         three_weeks_ago_avg = data[i - 3 * steps_per_week, :, 0]
-    #         one_day_ago_avg = data[i - args.steps_per_day, :, 0]
-    #         two_days_ago_avg = data[i - 2 * args.steps_per_day, :, 0]
-    #         three_days_ago_avg = data[i - 3 * args.steps_per_day, :, 0]
-    #         four_days_ago_avg = data[i - 4 * args.steps_per_day, :, 0]
-    #         five_daya_ago_avg = data[i - 5 * args.steps_per_day, :, 0]
-    #         weekly_avg_feature[i, :, 0] = (
-    #                                               last_week_avg + two_weeks_ago_avg + three_weeks_ago_avg + one_day_ago_avg
-    #                                               +two_days_ago_avg + three_days_ago_avg + four_days_ago_avg + five_daya_ago_avg
-    #                                       ) / 8
-    #     elif i >= 2 * steps_per_week:
-    #         # 用前两周数据 + 前两天的数据
-    #         last_week_avg = data[i - steps_per_week, :, 0]
-    #         two_weeks_ago_avg = data[i - 2 * steps_per_week, :, 0]
-    #         one_day_ago_avg = data[i - args.steps_per_day, :, 0]
-    #         two_days_ago_avg = data[i - 2 * args.steps_per_day, :, 0]
-    #         three_days_ago_avg = data[i - 3 * args.steps_per_day, :, 0]
-    #         four_days_ago_avg = data[i - 4 * args.steps_per_day, :, 0]
-    #         five_daya_ago_avg = data[i - 5 * args.steps_per_day, :, 0]
-    #         six_daya_ago_avg = data[i - 6 * args.steps_per_day, :, 0]
-    #         weekly_avg_feature[i, :, 0] = (
-    #                                               last_week_avg + two_weeks_ago_avg + one_day_ago_avg + two_days_ago_avg
-    #                                               + three_days_ago_avg + four_days_ago_avg + five_daya_ago_avg + six_daya_ago_avg
-    #                                       ) / 8
-    #     elif i >= steps_per_week:
-    #         # 用前一周数据 + 前三天的数据
-    #         last_week_avg = data[i - steps_per_week, :, 0]
-    #         one_day_ago_avg = data[i - args.steps_per_day, :, 0]
-    #         two_days_ago_avg = data[i - 2 * args.steps_per_day, :, 0]
-    #         three_days_ago_avg = data[i - 3 * args.steps_per_day, :, 0]
-    #         four_days_ago_avg = data[i - 4 * args.steps_per_day, :, 0]
-    #         five_daya_ago_avg = data[i - 5 * args.steps_per_day, :, 0]
-    #         six_daya_ago_avg = data[i - 6 * args.steps_per_day, :, 0]
-    #         seven_daya_ago_avg = data[i - 7 * args.steps_per_day, :, 0]
-    #         weekly_avg_feature[i, :, 0] = (
-    #                                               last_week_avg + one_day_ago_avg + two_days_ago_avg + three_days_ago_avg
-    #                                               + four_days_ago_avg + five_daya_ago_avg + six_daya_ago_avg + seven_daya_ago_avg
-    #                                       ) / 8
-    #     elif i >= args.steps_per_day:
-    #         # 使用前四天的时间片数据
-    #         previous_avgs = [data[i - j * args.steps_per_day, :, 0] for j in range(1, int(i / args.steps_per_day))]
-    #         last_time = [data[i - j , :, 0] for j in range(1, 9- int(i / args.steps_per_day))]
-    #         weekly_avg_feature[i, :, 0] = (sum(previous_avgs) +sum(last_time))/ 8
-    #     else:
-    #         # 不足四天的情况下，若没有可用数据则填充零
-    #         previous_avgs = [data[i - j, :, 0] for j in range(1, i + 1) if i - j >= 0]
-    #         if previous_avgs:
-    #             weekly_avg_feature[i, :, 0] = sum(previous_avgs) / len(previous_avgs)
-    #         else:
-    #             weekly_avg_feature[i, :, 0] = 0  # 没有历史数据时填充零
-    # for i in range(data.shape[0]):
-    #     if i >= 4 * steps_per_week:
-    #         # 获取过去四周同一时间片的平均值
-    #         last_week_avg = data[i - steps_per_week, :, 0]
-    #         two_weeks_ago_avg = data[i - 2 * steps_per_week, :, 0]
-    #         three_weeks_ago_avg = data[i - 3 * steps_per_week, :, 0]
-    #         four_weeks_ago_avg = data[i - 4 * steps_per_week, :, 0]
-    #         weekly_avg_feature[i, :, 0] = (
-    #                                               last_week_avg + two_weeks_ago_avg + three_weeks_ago_avg + four_weeks_ago_avg
-    #                                       ) / 4
-    #     elif i >= 3 * steps_per_week:
-    #         # 用前三周数据 + 前一天的数据
-    #         last_week_avg = data[i - steps_per_week, :, 0]
-    #         two_weeks_ago_avg = data[i - 2 * steps_per_week, :, 0]
-    #         three_weeks_ago_avg = data[i - 3 * steps_per_week, :, 0]
-    #         one_day_ago_avg = data[i - args.steps_per_day, :, 0]
-    #         weekly_avg_feature[i, :, 0] = (
-    #                                               last_week_avg + two_weeks_ago_avg + three_weeks_ago_avg + one_day_ago_avg
-    #                                       ) / 4
-    #     elif i >= 2 * steps_per_week:
-    #         # 用前两周数据 + 前两天的数据
-    #         last_week_avg = data[i - steps_per_week, :, 0]
-    #         two_weeks_ago_avg = data[i - 2 * steps_per_week, :, 0]
-    #         one_day_ago_avg = data[i - args.steps_per_day, :, 0]
-    #         two_days_ago_avg = data[i - 2 * args.steps_per_day, :, 0]
-    #         weekly_avg_feature[i, :, 0] = (
-    #                                               last_week_avg + two_weeks_ago_avg + one_day_ago_avg + two_days_ago_avg
-    #                                       ) / 4
-    #     elif i >= steps_per_week:
-    #         # 用前一周数据 + 前三天的数据
-    #         last_week_avg = data[i - steps_per_week, :, 0]
-    #         one_day_ago_avg = data[i - args.steps_per_day, :, 0]
-    #         two_days_ago_avg = data[i - 2 * args.steps_per_day, :, 0]
-    #         three_days_ago_avg = data[i - 3 * args.steps_per_day, :, 0]
-    #         weekly_avg_feature[i, :, 0] = (
-    #                                               last_week_avg + one_day_ago_avg + two_days_ago_avg + three_days_ago_avg
-    #                                       ) / 4
-    #     elif i >= steps_per_week:
-    #         # 使用前一周数据 + 前三天的数据
-    #         last_week_avg = data[i - steps_per_week, :, 0]
-    #         one_day_ago_avg = data[i - args.steps_per_day, :, 0]
-    #         two_days_ago_avg = data[i - 2 * args.steps_per_day, :, 0]
-    #         three_days_ago_avg = data[i - 3 * args.steps_per_day, :, 0]
-    #         weekly_avg_feature[i, :, 0] = (
-    #                                               last_week_avg + one_day_ago_avg + two_days_ago_avg + three_days_ago_avg
-    #                                       ) / 4
-    #     elif i >= 4:
-    #         # 使用前四天的时间片数据
-    #         previous_avgs = [data[i - j, :, 0] for j in range(1, 5)]
-    #         weekly_avg_feature[i, :, 0] = sum(previous_avgs) / 4
-    #     else:
-    #         # 不足四天的情况下，若没有可用数据则填充零
-    #         previous_avgs = [data[i - j, :, 0] for j in range(1, i + 1) if i - j >= 0]
-    #         if previous_avgs:
-    #             weekly_avg_feature[i, :, 0] = sum(previous_avgs) / len(previous_avgs)
-    #         else:
-    #             weekly_avg_feature[i, :, 0] = 0  # 没有历史数据时填充零
 
     steps_per_week = args.steps_per_day * 7
     past_week_avg_feature = np.zeros((L, N, F))
@@ -404,25 +184,8 @@ def get_dataloader(args, normalizer = 'std', tod=False, dow=False, weather=False
     x_weekly, y_weekly =  Add_Window_Horizon(weekly_avg_feature, args.lag, args.horizon, single)
     x_daily, y_daily =  Add_Window_Horizon(daily_avg_feature, args.lag, args.horizon, single)
     x_recent, y_recent =  Add_Window_Horizon(recent_avg_feature, args.lag, args.horizon, single)
-
-    # scaler = normalize_dataset(x_weekly, normalizer, args.column_wise)
-    # scaler1 = normalize_dataset(x_speed, normalizer, args.column_wise)
-    # scaler2 = normalize_dataset(x_occupy, normalizer, args.column_wise)
-    # # # scaler3 = normalize_dataset(y_speed, normalizer, args.column_wise)
-    # x_weekly = scaler.transform(x_weekly)
-    # x_speed = scaler1.transform(x_speed)
-    # x_occupy = scaler2.transform(x_occupy)
-    # y_speed = scaler3.transform(y_speed)
     x, y = np.concatenate([x, x_day, x_week, x_speed, x_occupy, x_weekly,x_daily,x_recent], axis=-1), np.concatenate([y, y_day, y_week], axis=-1) #B,T,N,3
-    #print("final - x:", x.shape, "y:", y.shape)
-    #print("输出拼接数据输入窗口的第四个维度第一个数据:", x[10, :, 0, 0])
-    #print("x1:t shape:", x[:, :, :, 1].shape)
-    #print("输出拼接数据输入窗口的第四个维度第二个数据:", x[10, :, 0, 1])
-    #print("x2:t shape:", x[:, :, :, 2].shape)
-    #print("x2:t shape:", x[:,:, 2, 2].shape)
-    #print("输出拼接数据输入窗口的第四个维度第三个数据:", x[:, :, 2, 2])
-    #print("输出拼接数据预测窗口的第四个维度的第二个数据:t shape:", y [10, :, 0, 0])
-    # spilit dataset by days or by ratio
+
     if args.test_ratio > 1:
         x_train, x_val, x_test = split_data_by_days(x, args.val_ratio, args.test_ratio)
         y_train, y_val, y_test = split_data_by_days(y, args.val_ratio, args.test_ratio)
@@ -440,11 +203,7 @@ def get_dataloader(args, normalizer = 'std', tod=False, dow=False, weather=False
     scaler3 = normalize_dataset(x_train[..., 5], normalizer, args.column_wise)
     scaler4 = normalize_dataset(x_train[..., 6], normalizer, args.column_wise)
     scaler5 = normalize_dataset(x_train[..., 7], normalizer, args.column_wise)
-    # scaler = normalize_dataset(x_train[..., :args.input_dim], normalizer, args.column_wise)
-    # # scaler1 = normalize_dataset(data_val, normalizer, args.column_wise)
-    # # scaler2 = normalize_dataset(data_test, normalizer, args.column_wise)
-    # # scaler3 = normalize_dataset(data, normalizer, args.column_wise)
-    #
+
     x_train[..., :args.input_dim] = scaler.transform(x_train[..., :args.input_dim])
     x_val[..., :args.input_dim] = scaler.transform(x_val[..., :args.input_dim])
     x_test[..., :args.input_dim] = scaler.transform(x_test[..., :args.input_dim])
@@ -463,12 +222,6 @@ def get_dataloader(args, normalizer = 'std', tod=False, dow=False, weather=False
     x_train[..., 7] = scaler5.transform(x_train[..., 7])
     x_val[..., 7] = scaler5.transform(x_val[..., 7])
     x_test[..., 7] = scaler5.transform(x_test[..., 7])
-    # y_train[...,:args.input_dim] = scaler.transform(y_train[...,:args.input_dim])
-    # y_val[...,:args.input_dim] = scaler.transform(y_val[...,:args.input_dim])
-    # y_test[...,:args.input_dim] = scaler.transform(y_test[...,:args.input_dim])
-    # data_train = np.concatenate([data_train,day_train,week_train], axis=-1)
-    # data_val = np.concatenate([data_val, day_val, week_val], axis=-1)
-    # data_test = np.concatenate([data_test, day_test, week_test], axis=-1)
 
     # #add time window
     # x_tra, y_tra = Add_Window_Horizon(data_train, args.lag, args.horizon, single)
